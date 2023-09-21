5a71e4a2bc1241f4c99a12310d6a76c9728fff4a
Safaith Hasan <safaith895@gmail.com>
2023-09-22 00:51:09 +0600

    moderateee

--- my_site/settings.py
+++ my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7yr-ndzr2mxz1lq81%zvws92f+@!x_g=@ckc0-kinvbhe+l#5c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
